8936b918427dc40da2c0c2588a561e4f38358c3a
Don-Quixote- <Don-Quixote-@users.noreply.github.com>
2015-04-14 13:33:47 -0700

    Update PEuler1.py

--- PEuler1.py
+++ PEuler1.py
@@ -1,9 +1,4 @@
-nums = list(range(1000))
-mtplrs = list()
-for x in nums:
+for x in range(1000):
 	if x % 3 == 0 or x % 5 == 0:
-		mtplrs.append(x)
-	else:
-		mtplrs.append('0')
-answer = sum(map(int, mtplrs))
+		answer+= x
 print answer
