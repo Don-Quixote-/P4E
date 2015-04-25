eac1db71075cf5e6be0f4d1251d9c2977f12d83f
Don_Quixote <Don-Quixote-@users.noreply.github.com>
2015-04-24 22:43:21 -0700

    Update PEuler9.py

--- PEuler9.py
+++ PEuler9.py
@@ -1,8 +1,6 @@
 for a in xrange(1000):
 	for b in xrange(a, 1000):
-		for c in xrange(b, 1000):
-			if a + b + c == 1000:
-				if a ** 2 + b ** 2 == c ** 2:
-					if a != b and b != c and c != a:
-						print (a*b*c)
-#It takes really long time to calculate. 
+		c = 1000 - b - a
+		if a ** 2 + b ** 2 == c ** 2:
+			if a != b and b != c and c != a:
+				print (a*b*c)
